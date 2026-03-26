6fadf28fdaecc5e49ed74877cce2b188618664fb
Vicky Shetty <vickyshetty@Vickys-Mac-mini.local>
2026-03-26 23:57:31 +0530

    initial: SmartPay 5-service microservices setup

---
+++ user-service/src/test/java/com/vicky/smartpay/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicky.smartpay.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
